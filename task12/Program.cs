a39c667526c0bf036fc983697f4afdcb5e3ad6a2
Ekaterina <c.hirko.va@hotmail.com>
2023-02-15 21:07:35 +0300

    task 12 modified

--- task12/Program.cs
+++ task12/Program.cs
@@ -5,24 +5,21 @@
 // 46 -> нет 
 // 161 -> да
 
-Console.WriteLine("Введите число A");
-int num = Convert.ToInt32(Console.ReadLine());
-if(num % 7 == 0 && num % 23 == 0) Console.WriteLine("Да");
-else Console.WriteLine("Нет");
-
-
-
-// Console.WriteLine("Введите число");
+// Console.WriteLine("Введите число A");
 // int num = Convert.ToInt32(Console.ReadLine());
 // if(num % 7 == 0 && num % 23 == 0) Console.WriteLine("Да");
 // else Console.WriteLine("Нет");
 
 
 
-// Задача 14
-// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.WriteLine("Введите число A");
+int number = Convert.ToInt32(Console.ReadLine());
 
-// 14 -> нет 
-// 46 -> нет 
-// 161 -> да
-
+if(number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("не кратно");
+}
