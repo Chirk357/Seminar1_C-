3cb51c38e7194133c3d9925544398272f5558f2f
Ekaterina <c.hirko.va@hotmail.com>
2023-02-15 20:39:05 +0300

    task 10 modified

--- task10/Program.cs
+++ task10/Program.cs
@@ -5,15 +5,39 @@
 // 918 -> 98
 
 
+// int number = Random.Shared.Next(100, 1000);
+// Console.WriteLine(number);
+
+// int numberA = number / 100;
+// Console.WriteLine(numberA);
+
+// int numberB = number % 10;
+// Console.WriteLine(numberB);
+
+// int numberC = numberA * 10;
+// int sum = numberC + numberB;
+// Console.WriteLine(sum);
+
+
+
+// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
 int number = Random.Shared.Next(100, 1000);
-Console.WriteLine(number);
+Console.WriteLine (number);
 
-int numberA = number / 100;
-Console.WriteLine(numberA);
+int digitone = number / 100;
+// Console.WriteLine (digitone);
 
-int numberB = number % 10;
-Console.WriteLine(numberB);
+int digittwo = number % 10;
+// Console.WriteLine (digittwo);
 
-int numberC = numberA * 10;
-int sum = numberC + numberB;
-Console.WriteLine(sum);
+int digitonemod = digitone * 10;
+// Console.WriteLine (digitonemod);
+
+int result = digitonemod + digittwo;
+Console.WriteLine (result);
