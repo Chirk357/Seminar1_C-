8e947ebbd9ba81c3864ea9ba7501a5ca52caefc4
Ekaterina <c.hirko.va@hotmail.com>
2023-02-17 21:38:40 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,7 @@
+﻿double a = 4;
+double b = 2;
+double c = Math.Pow(a , b); // можно написать Math.Pow(a * a, b) или Math.Pow(a / a, b). Нельзя положить в int. Только в double.
+Console.WriteLine(c); 
+
+double d = Math.Sqrt(c);
+Console.WriteLine(d);
