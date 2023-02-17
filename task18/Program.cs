304543f338b6311f8ac225b3ea6202282fde91bf
Ekaterina <c.hirko.va@hotmail.com>
2023-02-17 20:43:46 +0300

    task18_seminar3

---
+++ task18/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите координату точки по Х");
+int x = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите координату точки по Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > 0 && y > 0)
+{
+    Console.WriteLine("Точка в 1й четверти");
+}
+else if(x < 0 && y > 0)
+{
+    Console.WriteLine("Точка во 2й четверти");
+}
+else if(x < 0 && y < 0)
+{
+    Console.WriteLine("Точка в 3й четверти");
+}
+else if(x > 0 && y < 0)
+{
+    Console.WriteLine("Точка в 4й четверти");
+}
+else
+{
+    Console.WriteLine("Невозможно определить координатную четверть");
+}
