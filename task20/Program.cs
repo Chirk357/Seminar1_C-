6df8ea8bdf416cf32ad314f99fce5077316f379f
Ekaterina <c.hirko.va@hotmail.com>
2023-02-17 22:03:37 +0300

    task20

---
+++ task20/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 17
+// Методом , когда повторяются команды и создаем сами команду REadNumber. 
+// Мы пишем ReadNumber
+// Console.WriteLine("Введите координату точки по Х");
+// int x = Convert.ToInt32(Console.ReadLine()); 
+
+// Console.WriteLine("Введите координату точки по Y");
+// int y = Convert.ToInt32(Console.ReadLine()); 
+
+int ReadNumber(string message) || 
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+  
+}
+
+
+int x = ReadNumber("")
