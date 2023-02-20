ffe4a58391766708629097f7a28f2e5c065406f6
Ekaterina <c.hirko.va@hotmail.com>
2023-02-20 10:32:48 +0300

    task24

---
+++ task24_S2_HM_t13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: 
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i >= 1000; i++)
+{
+    Console.WriteLine(newNumber = number / 10);
+}
+Console.WriteLine(newNumber);
