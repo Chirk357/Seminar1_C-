714716a22e372a889208c9f1b013feb3918feda9
Ekaterina <c.hirko.va@hotmail.com>
2023-02-20 10:01:44 +0300

    task17 finalized

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+﻿// Задача 15: 
+// Напишите программу, которая принимает на вход цифру, 
 // обозначающую день недели, и проверяет, является ли этот день выходным.
 
 // 6 -> да 
@@ -7,4 +8,24 @@
 
 
 Console.WriteLine("Введите номер дня недели");
-int number = Convert.ToInt32((Console.ReadLine))
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 7 )
+{
+    
+    if (number == 6)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else if (number == 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день недели");
+    }
+}
+else{
+    Console.WriteLine("Такого дня недели не существует");
+}
