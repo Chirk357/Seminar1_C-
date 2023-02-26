7171a061adbf63eaab13717caa2936aefc7e14a4
Ekaterina <c.hirko.va@hotmail.com>
2023-02-26 15:04:05 +0300

    task38

---
+++ task38_S4_HM_t27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+     
+}
+
+int SumToN(int N);
+{
+   for(int i = 0; N =! 0; i++)
+    {
+    N = N % 10;
+    int sum = sum + N;
+
+    return sum;
+    }
+}
+
+int value = ReadNumber("введите число");
+int sum = SumToN(value);
