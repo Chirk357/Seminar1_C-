72b76748352229e67d73de8778871b9cd5db8d05
Ekaterina <c.hirko.va@hotmail.com>
2023-02-28 21:30:07 +0300

    task45

---
+++ task44_S6_t39/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+     
+}
+
+int[] GetRandomArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(leftRange, rightRange + 1);
+    }
+
+    return array;
+}
+
+const int LENGTH
---
+++ task45_S6_t42/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 42: 
+// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int[] GetRandomArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void ReverseArray(int[] array)
+{
+    for(int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+    }
+}
+
+const int LENGTH = 6;
+const int LEFTBORDER = -10;
+const int RIGHTBORDER = 10;
+
+int[] myArray = GetRandomArray(LENGTH, LEFTBORDER, RIGHTBORDER);
+Console.WriteLine(string.Join(", ", myArray));
+ReverseArray(myArray);
+Console.WriteLine(string.Join(", ", myArray));
