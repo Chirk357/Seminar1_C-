e64db79c164c6a873ff63e0231ecac9d4f0e281a
Ekaterina <c.hirko.va@hotmail.com>
2023-03-04 19:15:10 +0300

    task46.1_s6_t45 finished

---
+++ task46.1_S6_t45/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 45: 
+// Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
+
+int[] GetRandomArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+
+const int LENGTH = 6;
+const int LEFTBORDER = -10;
+const int RIGHTBORDER = 10;
+
+int[] myArray1 = GetRandomArray(LENGTH, LEFTBORDER, RIGHTBORDER);
+
+int[] myArray2 =new int[myArray1.Length];
+for(int i = 0; i < myArray2.Length; i++)
+{
+    myArray2[i] = myArray1[i];
+}
+
+// myArray1[0] = 10000000;
+Console.WriteLine(string.Join(", ", myArray1));
+Console.WriteLine(string.Join(", ", myArray2));
