e9fe2b8973785c04210b7d749dd314e0cb77978a
Ekaterina <c.hirko.va@hotmail.com>
2023-03-04 19:40:27 +0300

    task46.2_S6_HM_t41 finished

---
+++ task46.2_S6_HM_t41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+
+int count = 0;
+string stop = "stop";
+while(true)
+{
+    string userAnswer = Console.ReadLine()!;
+    if(userAnswer.ToLower() == stop)
+    {
+        break;
+    }
+    else
+    {
+        int number = Convert.ToInt32(userAnswer);
+        if(number > 0)
+        {
+            count++;
+        }
+    }
+}
+
+Console.WriteLine(count);
