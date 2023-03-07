a7305b5220fe2966f67dd917cb269ebbc6261c30
Ekaterina <c.hirko.va@hotmail.com>
2023-03-07 10:28:02 +0300

    task53_s7_hm_t47 finished

---
+++ task53_S7_HM_t47/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 47: 
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+double[,] GetRandomMatrix(int rows, int columns)
+{
+    
+    double[,] matrix = new double[rows, columns];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.NextDouble() * 10 - 4;
+            
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = ReadNumber("Введите количество строк:");
+int n = ReadNumber("Введите количество столбцов:");
+double[,] myMatrix = GetRandomMatrix(m, n);
+PrintMatrix(myMatrix);
