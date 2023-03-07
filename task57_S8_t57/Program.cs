eff000976c88540c097ddb56b655276ae2c3f9b4
Ekaterina <c.hirko.va@hotmail.com>
2023-03-07 22:40:31 +0300

    s8

---
+++ task57_S8_t57/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 57: 
+// Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+int[,] matrix = new int[,]
+{
+    {0,4,2,5,3}, 
+    {3,5,6,4,2},
+    {5,3,5,1,4}
+};
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[] CounterDictionary = new int[10];
+
+void Dictionary(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int k = 0; k < CounterDictionary.Length; k++)
+            {
+                if(matrix[i,j] == k)
+                {
+                    CounterDictionary[k] ++;
+                }
+            }
+        }
+    }
+}
+
+void PrinCounter(int[] dictionary)
+{
+    for(int i = 0; i < dictionary.Length; i++)
+    {
+        if(dictionary[i] != 0)
+        {
+            Console.WriteLine($"{i} -> {dictionary[i]}");
+        }
+    }
+}
+
+PrintMatrix(matrix);
+Dictionary(matrix);
+PrinCounter(CounterDictionary);
