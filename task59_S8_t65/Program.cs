f07744be55a7bf910ca77aa3e6529061bce5a36e
Ekaterina <c.hirko.va@hotmail.com>
2023-03-10 22:37:09 +0300

    seminar 9

---
+++ task59_S8_t65/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 65: 
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+
+void NaturalValues(uint n, uint m)
+{
+    if (n > m) Console.WriteLine("Число n не может быть больше m");
+    else if (m > n) NaturalValues(n, m - 1);
+    else Console.Write(m + " ");
+}
+
+NaturalValues(5, 10);
+Console.WriteLine();
+NaturalValues(6, 3);
---
+++ task60_S8_t67/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+int sum = 0;
+void SumOfNumbers(int n)
+{
+    if (n / 10 > 0)
+    {
+        SumOfNumbers(n / 10);
+    }
+    sum += n % 10;
+}
+
+SumOfNumbers(453);
+System.Console.WriteLine(sum);
