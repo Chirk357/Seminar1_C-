8dfa2dfcf946a21275802129bdc77556aaddc9c6
Ekaterina <c.hirko.va@hotmail.com>
2023-03-13 21:43:36 +0300

    8  finished

---
+++ task57.4_S8_HM_t58/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 58: 
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            Console.Write(matrix[j, i] + " ");
+            sum += matrix[j, i];
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] Fill(int length_x, int length_y, int leftnum = 0, int rightnum = 10)
+{
+    int[,] matrix = new int[length_y, length_x];
+    for (int i = 0; i < length_y; i++)
+    {
+        for (int j = 0; j < length_x; j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftnum, rightnum);
+        }
+    }
+    return matrix;
+}
+
+int[,] MatrixMultiply(int[,] matrix1, int[,] matrix2)
+{
+    int[,] MMatrix = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+    for (int i = 0; i < MMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < MMatrix.GetLength(1); j++)
+        {
+            MMatrix[i, j] = matrix1[i, j] * matrix2[i, j];
+        }
+    }
+    return MMatrix;
+}
+
+
+int[,] matrix1 = Fill(4, 4, 1, 10);
+int[,] matrix2 = Fill(4, 4, 1, 10);
+
+PrintMatrix(matrix1);
+Console.WriteLine();
+PrintMatrix(matrix2);
+Console.WriteLine();
+PrintMatrix(MatrixMultiply(matrix1, matrix2));
+
---
+++ task57.6_S8_HM_t62/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 62. 
+// Заполните спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            Console.Write(matrix[j, i] + " ");
+            sum += matrix[j, i];
+        }
+        // Console.Write($" -> sum:{sum}");
+        Console.WriteLine();
+    }
+}
+
+void SpiralFillMatrix(int[,] matrix)
+{
+    int[] rows = new int[16] {0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 2, 1, 1, 1, 2, 2};
+    int[] cols = new int[16] {0, 1, 2, 3, 3, 3, 3, 2, 1, 0, 0, 0, 1, 2, 2, 1};
+    int num = 1;
+    for (int i = 0; i < rows.Length; i++)
+    {
+        matrix[rows[i], cols[i]] = num;
+        num++;
+    }
+}
+
+int[,] matrix = new int[4, 4];
+SpiralFillMatrix(matrix);
+PrintMatrix(matrix);
